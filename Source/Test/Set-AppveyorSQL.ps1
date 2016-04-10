d2c4c11fac4de3cfd06872ffd8944a785887b1fc
Martin Pugh <martin@pughspace.com>
2016-04-10 10:53:53 -0400

    First pass at configuring SQL...

---
+++ Source/Test/Set-AppveyorSQL.ps1
@@ -0,0 +1,23 @@
+$instanceName = 'sql2014'
+$computerName = $env:COMPUTERNAME
+$smo = 'Microsoft.SqlServer.Management.Smo.'
+$wmi = New-Object ($smo + 'Wmi.ManagedComputer')
+
+# For the named instance, on the current computer, for the TCP protocol,
+# loop through all the IPs and configure them to use the standard port
+# of 1433.
+$uri = "ManagedComputer[@Name='$computerName']/ ServerInstance[@Name='$instanceName']/ServerProtocol[@Name='Tcp']"
+$Tcp = $wmi.GetSmoObject($uri)
+foreach ($ipAddress in $Tcp.IPAddresses)
+{
+    $ipAddress.IPAddressProperties["TcpDynamicPorts"].Value = ""
+    $ipAddress.IPAddressProperties["TcpPort"].Value = "1433"
+}
+$Tcp.IsEnabled = $true
+$Tcp.Alter()
+
+# Start services
+Set-Service SQLBrowser -StartupType Manual
+Start-Service SQLBrowser
+Get-Service "MSSQL`$$instanceName"
+Restart-Service "MSSQL`$$instanceName"
